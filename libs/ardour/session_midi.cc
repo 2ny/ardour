413ff597895a35352788e5c6ca20add49bf9d7e0
2ny <2nyst2@gmail.com>
2019-03-08 14:14:11 +0100

    do not disconnect MidiPortSelection-flagged ports from everything when (MIDI track) selection changes
    If the user manually connects such a port to something, then it is
    up the user to disconnect it too
    
    (backport of f1dfc6d and 8b212bf to last stable version)

--- libs/ardour/session_midi.cc
+++ libs/ardour/session_midi.cc
@@ -764,8 +764,20 @@ Session::rewire_selected_midi (boost::shared_ptr<MidiTrack> new_midi_target)
 		}
 
 		for (vector<string>::const_iterator p = msp.begin(); p != msp.end(); ++p) {
-			/* disconnect the port from everything */
-			AudioEngine::instance()->disconnect (*p);
+      /* backport commits f1dfc6d and 8b212bf*/
+      PortManager::MidiPortInformation mpi (AudioEngine::instance()->midi_port_information (*p));
+
+			/* if a port is marked for control data, do not
+			 * disconnect it from everything since it may also be
+			 * used via a control surface or some other
+			 * functionality.
+			 */
+
+			if (!(mpi.properties & MidiPortControl)) {
+				/* disconnect the port from everything */
+				AudioEngine::instance()->disconnect (*p);
+			}
+
 			/* connect it to the new target */
 			new_midi_target->input()->connect (new_midi_target->input()->nth(0), (*p), this);
 		}
